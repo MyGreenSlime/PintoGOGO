7c0d0f02eae811827a1712cb6c0cdeaa86ee4d37
Nstw <name.sp926@gmail.com>
2018-10-03 17:44:34 +0700

    create navbar/carousel/choice

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,23 @@
-import React, { Component } from 'react';
-import './css/App.css';
-import { Button } from 'reactstrap';
-import { Route, Switch } from 'react-router-dom'
+import React, { Component } from "react";
+import "./css/App.css";
+import { Route, Switch } from "react-router-dom";
 
-import Menu from './component/menu'
-const Home = () => <h1>Home</h1>
+import Navbar from "./component/navbar/navbar";
+import Carousel from "./component/carousel/carousel";
+import Choice from "./component/choice/choice";
 
-{/*-------------Add path of page---------------*/}
+const Home = () => <h1>Home</h1>;
+
+/*-------------Add path of page---------------*/
 class App extends Component {
   render() {
     return (
-
-      <div className="App container">
+      <div className="App">
+        <Navbar />
+        <Carousel />
+        <Choice />
         <Switch>
           <Route exact path="/" component={Home} />
-          <Route path="/about" component={Menu} />
         </Switch>
       </div>
     );
