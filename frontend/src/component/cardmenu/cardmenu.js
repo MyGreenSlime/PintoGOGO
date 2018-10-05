5ad4b597a4289bf20c1d8c5590158985eedb6147
Weeya Weerawatpruchya <weeyaa.w@gmail.com>
2018-10-05 17:24:29 +0700

    add to card, menu move

--- frontend/src/component/cardmenu/cardmenu.js
+++ frontend/src/component/cardmenu/cardmenu.js
@@ -1,26 +1,53 @@
 import React, { Component} from 'react';
 import '../cardmenu/cardmenu.css';
-import { Container, Row, Col,CardImg } from 'reactstrap';
-class cardMenu extends Component {
+import { Container, Row, Col} from 'reactstrap';
+
+ 
+export default class cardMenu extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+          clicked : 0
+        }
+      }
+
+    imageClick(e){
+        console.log('Click!!!!');
+        this.setState({
+            clicked : this.state.clicked+1
+        })
+        e.preventDefault();
+    }  
     
-    render() {
-        
+    render() { 
         return (
-            
             <div>
-                <div className="picture">
-                    {/* change height->auto */}
-                    <CardImg src={this.props.picture} height="170" width="170"/>
-                </div>
-                <div className="textunderpicture">
-                    <p>{this.props.name}<br/>
-                    {this.props.calories} Kcal</p>
+                
+                <card>
+                    <img width="70%" src={this.props.picture} className="picture"/>
+                </card>
+                
+                <div className="undermenu">
+                    <div className="textundermenu">
+                        <p>{this.props.name}<br/>
+                        {this.props.calories} Kcal</p>
+                    </div>
+                    <div className="cartbutton"  onClick ={this.imageClick.bind(this)}>
+                        <img src={"/img/other/cart.png"} height="20"/>
+                            {
+                            // this.state.clicked 
+                            // <div>You clicked me!</div>
+                            }
+                    </div>  
                 </div>
                 
+                {/* <p>total click: {this.state.clicked}</p> */}
+            
             </div>
-            );
-        }
+            
+        );
     }
-    
+}
 
-export default cardMenu;
+
+
--- frontend/src/component/menu/menu.js
+++ frontend/src/component/menu/menu.js
@@ -10,8 +10,17 @@ class Menu extends Component {
     this.state = {
       menus: {},
       isLoaded: false,
+      firstImg: 0,
+      secondImg: 1,
+      thirdImg: 2,
+      forthImg: 3,
+      fifthImg: 4,
+      sixthImg: 5,
     }
+
+    
   }
+
 
   componentDidMount() {
     fetch('http://localhost:4000/menus/food')
@@ -25,34 +34,69 @@ class Menu extends Component {
     
   }
 
+  rightClick(e){
+    console.log('Click!!!!');
+    this.setState({
+      firstImg: this.state.firstImg+6,
+      secondImg: this.state.secondImg+6,
+      thirdImg: this.state.thirdImg+6,
+      forthImg: this.state.forthImg+6,
+      fifthImg: this.state.fifthImg+6,
+      sixthImg: this.state.sixthImg+6,
+    })
+    e.preventDefault();
+  }  
+
+  leftClick(e){
+    console.log('Click!!!!');
+    this.setState({
+      firstImg: this.state.firstImg-6,
+      secondImg: this.state.secondImg-6,
+      thirdImg: this.state.thirdImg-6,
+      forthImg: this.state.forthImg-6,
+      fifthImg: this.state.fifthImg-6,
+      sixthImg: this.state.sixthImg-6,
+    })
+    e.preventDefault();
+  }
+
   render() {
     
-    var {isLoaded, menus} = this.state;
+    var {isLoaded, menus, firstImg, secondImg, thirdImg, forthImg, fifthImg, sixthImg} = this.state;
+    
     if(!isLoaded) {
       return <div>loading....</div>
     }
       return (
         <div className="menuzone"> 
+        <div className="rightClickMenu"  onClick ={this.rightClick.bind(this)}>
+          <img src={"/img/other/right-arrow.png"} height="20"/>
+        </div>  
+
+        <div className="leftClickMenu"  onClick ={this.leftClick.bind(this)}>
+          <img src={"/img/other/left-arrow.png"} height="20"/>
+        </div>  
           <Row className="firstrow">
             <Col >
-              <CardMenu name={menus[0].menu_name} picture={menus[0].img_url} calories={menus[0].calories}/>
+            {menus[firstImg] &&<CardMenu name={menus[firstImg].menu_name} picture={menus[firstImg].img_url} calories={menus[firstImg].calories}/> }
             </Col>
             <Col >
-              <CardMenu name={menus[1].menu_name} picture={menus[1].img_url} calories={menus[1].calories}/>
+            {menus[secondImg] &&<CardMenu name={menus[secondImg].menu_name} picture={menus[secondImg].img_url} calories={menus[secondImg].calories}/> }
             </Col>
             <Col >
-            <CardMenu name={menus[2].menu_name} picture={menus[2].img_url} calories={menus[2].calories}/>
+            {menus[thirdImg] && <CardMenu name={menus[thirdImg].menu_name} picture={menus[thirdImg].img_url} calories={menus[thirdImg].calories}/> }
             </Col>
+            
           </Row>
           <Row className="secondrow">
             <Col >
-              <CardMenu name={menus[3].menu_name} picture={menus[3].img_url} calories={menus[3].calories}/>
+            {menus[forthImg] && <CardMenu name={menus[forthImg].menu_name} picture={menus[forthImg].img_url} calories={menus[forthImg].calories}/> } 
             </Col>
             <Col>
-              <CardMenu name={menus[4].menu_name} picture={menus[4].img_url} calories={menus[4].calories}/>
+            {menus[fifthImg] &&<CardMenu name={menus[fifthImg].menu_name} picture={menus[fifthImg].img_url} calories={menus[fifthImg].calories}/> }
             </Col>
             <Col>
-            <CardMenu name={menus[5].menu_name} picture={menus[5].img_url} calories={menus[5].calories}/>
+            {menus[sixthImg] &&<CardMenu name={menus[sixthImg].menu_name} picture={menus[sixthImg].img_url} calories={menus[sixthImg].calories}/> }
             </Col>
           </Row>
           
