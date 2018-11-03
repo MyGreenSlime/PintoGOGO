54034805b6c2e3ee7a7fe274baa842f024dfe8b0
PattarapornH <pataraporn.han@gmail.com>
2018-11-03 16:48:07 +0700

    fix bug arrow and add loading

--- frontend/src/component/snack/snack.js
+++ frontend/src/component/snack/snack.js
@@ -1,10 +1,17 @@
-import React, { Component} from 'react';
-import { Card, CardImg, CardText, CardBody,
-  CardTitle, CardSubtitle, Button } from 'reactstrap';
-import { Row } from 'reactstrap';
-import CardMenu from '../cardsnack/cardsnack';
-import '../snack/snack.css';
-import axios from 'axios';
+import React, { Component } from "react";
+import {
+  Card,
+  CardImg,
+  CardText,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  Button
+} from "reactstrap";
+import { Row } from "reactstrap";
+import CardMenu from "../cardsnack/cardsnack";
+import "../snack/snack.css";
+import axios from "axios";
 
 class Snack extends Component {
   constructor(props) {
@@ -19,40 +26,42 @@ class Snack extends Component {
       fifthImg: 4,
       sixthImg: 5,
       length_snack: 0
-    }
+    };
     this.checkFirstSnackSet = this.checkFirstSnackSet.bind(this);
     this.checkLastSnackSet = this.checkLastSnackSet.bind(this);
   }
 
   componentDidMount() {
-    axios.get("/api/menus/snack")
+    axios
+      .get("/api/menus/snack")
       .then(response => {
         this.setState({
-           isLoaded: true, 
-           snacks: response.data
+          isLoaded: true,
+          snacks: response.data
         });
       })
-      .then(this.setState({
-          length_snack : Object.keys(this.state.snacks).length
-      }));    
-    
+      .then(
+        this.setState({
+          length_snack: Object.keys(this.state.snacks).length
+        })
+      );
   }
 
-  rightClick(e){
-    console.log('Click!!!!');
+  rightClick(e) {
+    console.log("Click!!!!");
     this.setState({
-      firstImg: this.state.firstImg+6,
-      secondImg: this.state.secondImg+6,
-      thirdImg: this.state.thirdImg+6,
-      forthImg: this.state.forthImg+6,
-      fifthImg: this.state.fifthImg+6,
-      sixthImg: this.state.sixthImg+6,
-    })
+      firstImg: this.state.firstImg + 6,
+      secondImg: this.state.secondImg + 6,
+      thirdImg: this.state.thirdImg + 6,
+      forthImg: this.state.forthImg + 6,
+      fifthImg: this.state.fifthImg + 6,
+      sixthImg: this.state.sixthImg + 6
+    });
     e.preventDefault();
-  }  
+  }
 
-  leftClick(e){
-    console.log('Click!!!!');
+  leftClick(e) {
+    console.log("Click!!!!");
     if (this.state.firstImg - 6 >= 0) {
       this.setState({
         firstImg: this.state.firstImg - 6,
@@ -60,72 +69,94 @@ class Snack extends Component {
         thirdImg: this.state.thirdImg - 6,
         forthImg: this.state.forthImg - 6,
         fifthImg: this.state.fifthImg - 6,
-        sixthImg: this.state.sixthImg - 6,
-      })
+        sixthImg: this.state.sixthImg - 6
+      });
     }
     e.preventDefault();
   }
 
   checkFirstSnackSet() {
+    let img = "";
     if (this.state.firstImg - 6 >= 0) {
-      return <img src={"/img/other/left-arrow.png"} height="20" />;
+      img = <img src="/img/other/left-arrow.png" height="20" />;
     }
+    console.log("left ", img);
+    return img;
   }
 
   checkLastSnackSet() {
-    if (this.state.firstImg <= this.state.length_snack) {
-      return <img className="imgbutton" src={"/img/other/right-arrow.png"} height="20" />
+    let img = "";
+    if (
+      this.state.firstImg <= this.state.snacks.length &&
+      this.state.secondImg <= this.state.snacks.length &&
+      this.state.thirdImg <= this.state.snacks.length &&
+      this.state.forthImg <= this.state.snacks.length &&
+      this.state.fifthImg <= this.state.snacks.length &&
+      this.state.sixthImg <= this.state.snacks.length &&
+      this.state.snacks.length != 0
+    ) {
+      img = (
+        <img
+          className="imgbutton"
+          src="/img/other/right-arrow.png"
+          height="20"
+        />
+      );
     }
+    console.log("right ", img);
+    return img;
   }
 
   render() {
-    
-    const { 
-      isLoaded, 
-      snacks, 
+    const {
+      isLoaded,
+      snacks,
       firstImg,
-      secondImg, 
-      thirdImg, 
-      forthImg, 
-      fifthImg, 
+      secondImg,
+      thirdImg,
+      forthImg,
+      fifthImg,
       sixthImg
     } = this.state;
-    
+
     if (!isLoaded) {
-      return <div>loading....</div>
+      return <div class="loader setposition"/>;
     }
     return (
       <div className="snackzone">
-
         <div className="mergerow-left__snack">
-          <div onClick={this.leftClick.bind(this)} >
+          <div onClick={this.leftClick.bind(this)}>
             {this.checkFirstSnackSet()}
           </div>
         </div>
-        
+
         <Row className="firstrow">
+          {snacks[firstImg] && (
+            <CardMenu
+              name={snacks[firstImg].snack_name}
+              picture={snacks[firstImg].img_url}
+              calories={snacks[firstImg].calories}
+              id={snacks[firstImg]._id}
+            />
+          )}
 
-          {snacks[firstImg] && <CardMenu 
-            name={snacks[firstImg].snack_name} 
-            picture={snacks[firstImg].img_url} 
-            calories={snacks[firstImg].calories} 
-            id = {snacks[firstImg]._id}/>
-          }
+          {snacks[secondImg] && (
+            <CardMenu
+              name={snacks[secondImg].snack_name}
+              picture={snacks[secondImg].img_url}
+              calories={snacks[secondImg].calories}
+              id={snacks[secondImg]._id}
+            />
+          )}
 
-          {snacks[secondImg] && <CardMenu 
-            name={snacks[secondImg].snack_name} 
-            picture={snacks[secondImg].img_url} 
-            calories={snacks[secondImg].calories} 
-            id = {snacks[secondImg]._id} />
-          }
-
-          {snacks[thirdImg] && <CardMenu 
-            name={snacks[thirdImg].snack_name} 
-            picture={snacks[thirdImg].img_url} 
-            calories={snacks[thirdImg].calories} 
-            id = {snacks[thirdImg]._id} />
-          }
-
+          {snacks[thirdImg] && (
+            <CardMenu
+              name={snacks[thirdImg].snack_name}
+              picture={snacks[thirdImg].img_url}
+              calories={snacks[thirdImg].calories}
+              id={snacks[thirdImg]._id}
+            />
+          )}
         </Row>
 
         <div className="mergerow-right__snack">
@@ -135,33 +166,37 @@ class Snack extends Component {
         </div>
 
         <Row className="secondrow">
-          {snacks[forthImg] && <CardMenu 
-            name={snacks[forthImg].snack_name} 
-            picture={snacks[forthImg].img_url} 
-            calories={snacks[forthImg].calories} 
-            id = {snacks[forthImg]._id}/>
-          }
+          {snacks[forthImg] && (
+            <CardMenu
+              name={snacks[forthImg].snack_name}
+              picture={snacks[forthImg].img_url}
+              calories={snacks[forthImg].calories}
+              id={snacks[forthImg]._id}
+            />
+          )}
 
-          {snacks[fifthImg] && <CardMenu 
-            name={snacks[fifthImg].snack_name} 
-            picture={snacks[fifthImg].img_url} 
-            calories={snacks[fifthImg].calories} 
-            id = {snacks[fifthImg]._id}/>
-          }
+          {snacks[fifthImg] && (
+            <CardMenu
+              name={snacks[fifthImg].snack_name}
+              picture={snacks[fifthImg].img_url}
+              calories={snacks[fifthImg].calories}
+              id={snacks[fifthImg]._id}
+            />
+          )}
 
-          {snacks[sixthImg] && <CardMenu 
-            name={snacks[sixthImg].snack_name} 
-            picture={snacks[sixthImg].img_url} 
-            calories={snacks[sixthImg].calories} 
-            id = {snacks[sixthImg]._id}/>
-          }
-
+          {snacks[sixthImg] && (
+            <CardMenu
+              name={snacks[sixthImg].snack_name}
+              picture={snacks[sixthImg].img_url}
+              calories={snacks[sixthImg].calories}
+              id={snacks[sixthImg]._id}
+            />
+          )}
         </Row>
-        <div></div>
+        <div />
       </div>
-      );
-    }
-    
+    );
   }
+}
 
 export default Snack;
