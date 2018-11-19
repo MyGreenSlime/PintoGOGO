f9ccf01c188f1d3ba00f6fbbc40eaf67571bb055
Nstw <name.sp926@gmail.com>
2018-11-19 15:27:55 +0700

    recommend v.1.1: responsive fixed

--- frontend/src/component/recommend/recommend.js
+++ frontend/src/component/recommend/recommend.js
@@ -1,59 +1,55 @@
 import React, { Component } from "react";
 import "../recommend/style-recommend.css";
-import axios from 'axios';
+import axios from "axios";
 
 export default class Recommend extends Component {
-
   constructor() {
     super();
     this.state = {
       menus: [],
-			isLoaded: false
-    }
+      isLoaded: false
+    };
   }
 
   componentDidMount() {
-    axios.get('/api/menus/food')
-    .then(res => {
-      this.setState({ 
-				isLoaded: true, 
-				menus: res.data,
-			});
-		})
-		.then(() => {
-			console.log(this.state.menus)
-		});
+    axios
+      .get("/api/menus/food")
+      .then(res => {
+        this.setState({
+          isLoaded: true,
+          menus: res.data
+        });
+      })
+      .then(() => {
+        console.log("menu: ", this.state.menus);
+      });
   }
   render() {
-    const {	menus,
-      isLoaded, 
-    } = this.state;
+    const { menus, isLoaded } = this.state;
 
     if (!!!isLoaded) {
-      return <React.Fragment />
+      return <React.Fragment />;
     }
-    {console.log("menu:",menus)}
+
     return (
-      <section className="recommend__block" fluid>
-        <div className="set-recommend">
-          <b style={{fontSize: "20px"}}>Recommended</b>
-          <div className="grid-recommend">
-            <div>
-              <img className="img-rec" src={menus[1].img_url} />
-               {menus[1].menu_name}
-            </div>
-            <div>
-              <img className="img-rec" src={menus[3].img_url} />
-              {menus[3].menu_name}
-            </div>
-            <div>
-              <img className="img-rec" src={menus[2].img_url} />
-              {menus[2].menu_name}
-            </div>
-            <div>
-              <img className="img-rec" src={menus[0].img_url} />
-              {menus[0].menu_name}
-            </div>
+      <section className="recommend__block">
+        <p className="txt__rec">Recommended</p>
+        <div className="row">
+          <div className="col-lg-3 col-md-6 nopadding">
+            <img className="img-rec" src={menus[1].img_url} />
+            <p>{menus[1].menu_name}</p>
+          </div>
+          <div className="col-lg-3 col-md-6 nopadding">
+            <img className="img-rec" src={menus[3].img_url} />
+            <p>{menus[3].menu_name}</p>
+          </div>
+          <div className="col-lg-3 col-md-6 nopadding">
+            <img className="img-rec" src={menus[2].img_url} />
+            <p>{menus[2].menu_name}</p>
+          </div>
+          <div className="col-lg-3 col-md-6 nopadding">
+            <img className="img-rec" src={menus[0].img_url} />
+            <p>{menus[0].menu_name}</p>
           </div>
         </div>
       </section>
