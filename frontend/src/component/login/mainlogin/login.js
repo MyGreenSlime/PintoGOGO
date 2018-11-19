3a700a20d8250445155ea25316d094dc9b9f1afe
PattarapornH <pataraporn.han@gmail.com>
2018-11-19 11:31:34 +0700

    responsive login

--- frontend/src/component/login/mainlogin/login.js
+++ frontend/src/component/login/mainlogin/login.js
@@ -1,10 +1,6 @@
 import React, { Component } from "react";
 import Userpass from "../userpass/userpass";
-import Googlelogin from "../google/googlelogin";
-import Facebooklogin from "../facebook/facebooklogin";
 import '../mainlogin/style-login.css'
-import "../google/style-google.css";
-import '../facebook/style-facebook.css'
 import '../userpass/style-userpass.css'
 
 class Login extends Component {
@@ -18,9 +14,7 @@ class Login extends Component {
       <React.Fragment>
         <div className="login__box__container">
           <div className="login__box">
-            <Userpass />
-            {/* <Googlelogin /> */}
-            {/* <Facebooklogin /> */}
+            <Userpass />  
           </div>
         </div>
       </React.Fragment>
