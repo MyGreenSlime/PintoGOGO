d9aa12750193e225ad6cda8a475b796e985e8d7c
PattarapornH <pataraporn.han@gmail.com>
2018-11-22 00:01:54 +0700

    get package & profile

--- frontend/src/component/api/api.js
+++ frontend/src/component/api/api.js
@@ -24,9 +24,8 @@
 import axios from 'axios'
 
 export function getFoodOrSnack(menu, isLoaded, path) {
-  console.log("get menu outside axios")
   axios.get("/api/menus/" + path).then(res => {
-    console.log("get menu inside axios")
+    console.log("get ",menu)
     this.setState({
       [menu]: res.data,
       [isLoaded]: true
@@ -34,3 +33,23 @@ export function getFoodOrSnack(menu, isLoaded, path) {
   }).then(()=>{console.log("allmenus ",this.state[menu])})
 }
 
+export function getPackage(pack, isLoaded, path){
+  axios.get("api/packages"+path).then(res => {
+    console.log("get package")
+    this.setState({
+      [pack] : res.data,
+      [isLoaded]: true
+    });
+  }).then(() => {console.log("package ",this.state[pack])})
+}
+
+export function getProfile(profile, isLoaded) {
+  axios.get('/api/users/profile')
+    .then(res => {
+      console.log("get profile")
+      this.setState({
+        [profile]: res.data,
+        [isLoaded]: true
+      });
+  }).then( () => {console.log(this.state[profile])});
+}
