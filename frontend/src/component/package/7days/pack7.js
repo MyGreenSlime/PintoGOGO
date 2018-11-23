79c8605bf7db54178d02176b9dbcd0e800629dda
PattarapornH <pataraporn.han@gmail.com>
2018-11-23 18:41:19 +0700

    reuse component 3/5/7

--- frontend/src/component/package/7days/pack7.js
+++ frontend/src/component/package/7days/pack7.js
@@ -1,71 +1,16 @@
-import React, {Component} from 'react';
-import '../package.css';
-import axios from "axios";
-import NoPackage from '../nopackage';
-import LinkWithPrev from '../../LinkWithPrev/linkwithprev.js'
 
-export default class Pack7 extends Component {
-	constructor(props) {
-    super(props);
-    this.state = {
-      packages: [],
-      isLoaded: false, 
-    }
-		this.createDivPackage = this.createDivPackage.bind(this);
-	}
+import React, { Component } from 'react';
+import SavedPack from '../savepack';
 
-  componentDidMount() {
-    axios.get('api/packages/system/7days')
-    .then(res => {
-      this.setState ({
-        packages: res,
-        isLoaded: true,
-      });
-    })
-    .then(() => {console.log("package", this.state.packages.data)})
-  }
-
-  createDivPackage(curPack) {
-    let divpk = 
-    <React.Fragment>
-      <div className="set-each-package row">
-			<div className="col-sm-4">
-				<img className="img-pack" src={curPack.day_meal[0].meal_1.img_url} />
-			</div>
-			<div className="col-sm dis-grid">
-				<div>
-					<p className="name-each-pks">{curPack.name_package}</p>
-					<p>{curPack.description}</p>
-				</div>
-				<LinkWithPrev to={"/7days/"+curPack._id}>
-					<button className="btn view-pks">View Package</button>
-				</LinkWithPrev>
-			</div>
-      </div>
-		</React.Fragment>
-		return divpk;
-	}
-
+class Pack3 extends Component {
 	render() {
-
-		if(!this.state.isLoaded){
-      return <div className="loader" />;         
-		}
-		if(!this.state.packages.data[0]) {
-      return <NoPackage />
-    }
-
-		const listPackages = this.state.packages.data.map((pk,index) => 
-			<div key={index}>
-				{this.createDivPackage(pk)}
-			</div>
-		);
-		console.log(typeof this.state.packages.data)
-
 		return (
 			<React.Fragment>
-				{listPackages}
+				<SavedPack
+					path="7days" />
 			</React.Fragment>
 		);
 	}
 }
+
+export default Pack3;
---
+++ frontend/src/component/package/savepack.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react';
+import './package.css';
+import axios from "axios";
+import NoPackage from './nopackage';
+import LinkWithPrev from '../LinkWithPrev/linkwithprev.js'
+import { getPackage } from '../api/api';
+
+export default class Savepack extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      packages: [],
+      isLoaded: false,
+    }
+    this.createDivPackage = this.createDivPackage.bind(this);
+  }
+
+  componentDidMount() {
+    axios.get('api/packages/system/'+this.props.path)
+      .then(res => {
+          this.setState({
+              packages: res,
+              isLoaded: true,
+          });
+      })
+      .then(() => { console.log("package", this.state.packages.data) })
+  }
+
+  createDivPackage(curPack) {
+    let divpk =
+      <React.Fragment>
+        <div className="set-each-package row">
+          <div className="col-sm-4">
+            <img className="img-pack" src={curPack.day_meal[0].meal_1.img_url} />
+          </div>
+          <div className="col-sm dis-grid">
+            <div>
+              <p className="name-each-pks">{curPack.name_package}</p>
+              <p>{curPack.description}</p>
+            </div>
+            <LinkWithPrev to={"/" + this.props.path + "/" + curPack._id}>
+              <button className="btn view-pks">View Package</button>
+            </LinkWithPrev>
+          </div>
+        </div>
+      </React.Fragment>
+    return divpk;
+  }
+
+    render() {
+
+        if (!this.state.isLoaded) {
+            return <div className="loader" />;
+        }
+        if (!this.state.packages.data[0]) {
+            return <NoPackage />
+        }
+
+        const listPackages = this.state.packages.data.map((pk, index) =>
+            <div key={index}>
+                {this.createDivPackage(pk)}
+            </div>
+        );
+        console.log(typeof this.state.packages.data)
+
+        return (
+            <React.Fragment>
+                {listPackages}
+            </React.Fragment>
+        );
+    }
+}
