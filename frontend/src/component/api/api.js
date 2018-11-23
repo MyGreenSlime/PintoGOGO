049bd196541c75e8a6772aaa6a141ef6f45b13ac
PattarapornH <pataraporn.han@gmail.com>
2018-11-23 17:19:03 +0700

    add menu/snack post

--- frontend/src/component/api/api.js
+++ frontend/src/component/api/api.js
@@ -1,33 +1,61 @@
+import axios from "axios";
 
-import axios from 'axios'
-
+/** GET **/
 export function getFoodOrSnack(menu, isLoaded, path) {
-  axios.get("/api/menus/" + path).then(res => {
-    console.log("get ",menu)
-    this.setState({
-      [menu]: res.data,
-      [isLoaded]: true
+  axios
+    .get("/api/menus/" + path)
+    .then(res => {
+      console.log("get ", menu);
+      this.setState({
+        [menu]: res.data,
+        [isLoaded]: true
+      });
+    })
+    .then(() => {
+      console.log("allmenus ", this.state[menu]);
     });
-  }).then(()=>{console.log("allmenus ",this.state[menu])})
 }
 
-export function getPackage(pack, isLoaded, path){
-  axios.get("api/packages"+path).then(res => {
-    console.log("get package")
-    this.setState({
-      [pack] : res.data,
-      [isLoaded]: true
+export function getPackage(pack, isLoaded, path) {
+  axios
+    .get("api/packages" + path)
+    .then(res => {
+      console.log("get package");
+      this.setState({
+        [pack]: res.data,
+        [isLoaded]: true
+      });
+    })
+    .then(() => {
+      console.log("package ", this.state[pack]);
     });
-  }).then(() => {console.log("package ",this.state[pack])})
 }
 
 export function getProfile(profile, isLoaded) {
-  axios.get('/api/users/profile')
+  axios
+    .get("/api/users/profile")
     .then(res => {
-      console.log("get profile")
+      console.log("get profile");
       this.setState({
         [profile]: res.data,
         [isLoaded]: true
       });
-  }).then( () => {console.log("user ",this.state[profile])});
+    })
+    .then(() => {
+      console.log("user ", this.state[profile]);
+    });
+}
+
+/** POST **/
+export function addFoodOrSnack(data, status, path) {
+  axios.post("/api/menus/" + path + "/add", data)
+    .then(res => {
+      this.setState({
+        [status]: res.data.ok
+      });
+    })
+    .then(() => {
+      console.log("redirect");
+      this.renderRedirect();
+    });
 }
