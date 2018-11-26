aaca91d7a963c89939c28a42d5d0e341b8b65e12
Nstw <name.sp926@gmail.com>
2018-11-26 18:18:36 +0700

    edit small pieces (backend)

--- Backend/controllers/address_control.js
+++ Backend/controllers/address_control.js
@@ -1,29 +1,29 @@
-const Address = require('../models/address');
+const Address = require("../models/address");
 
 exports.getAllAddress = (req, res) => {
-    const error = {}
-    if(!req.user.type){
-        error.admin = "need admin"
-        res.status(500).send(error)
+  const error = {};
+  if (!req.user.type) {
+    error.admin = "need admin";
+    res.status(500).send(error);
+  }
+  Address.find({}, function(err, address) {
+    if (err) {
+      error.address = err;
+      res.status(500).send(error);
+    } else {
+      res.json(address);
     }
-    Address.find({}, function(err, address){
-        if(err) {
-            error.address = err
-            res.status(500).send(error);
-        } else {
-            res.send(address);
-        }
-    });
-}
+  });
+};
 
 exports.getCurrentAddress = (req, res) => {
-    const error = {}
-    Address.findOne({owner : req.user.user_name}, function(err, address){
-        if(err) {
-            error.address = err
-            res.status(500).send(error);
-        } else {
-            res.send(address);
-        }
-    });
-}
+  const error = {};
+  Address.find({ owner: req.user.user_name }, function(err, address) {
+    if (err) {
+      error.address = err;
+      res.status(500).send(error);
+    } else {
+      res.json(address);
+    }
+  });
+};
--- Backend/controllers/bill_control.js
+++ Backend/controllers/bill_control.js
@@ -1,34 +1,41 @@
-const Order =  require('../models/order');
-const Bill = require('../models/bill');
+const Order = require("../models/order");
+const Bill = require("../models/bill");
 
 exports.getAllBill = (req, res) => {
-    const error = {}
-    if(!req.user.type) {
-        error.admin = "need admin account"
-        res.status(500).send(error)
-    }
-    Bill.find({})
-        .populate({path : "order", model : "Order" })
-        .exec((err, bill) => {
-            if(err) {
-                error.bills = err
-                res.status(500).send(err)
-            } else {
-                res.json(bill)
-            }
-        })       
-}
+  const error = {};
+  if (!req.user.type) {
+    error.admin = "need admin account";
+    res.status(500).send(error);
+  }
+  Bill.find({})
+    .populate({ path: "order", model: "Order" })
+    .exec((err, bill) => {
+      if (err) {
+        error.bills = err;
+        res.status(500).send(err);
+      } else {
+        res.json(bill);
+      }
+    });
+};
 
 exports.getCurrentBill = (req, res) => {
-    const error = {}
-    Bill.findOne({user : req.user.id, isfinish : false})
-        .populate({path : "order", model : "Order" })
-        .exec((err, bill) => {
-            if(err) {
-                error.bills = err
-                res.status(500).json(error)
-            } else {
-                res.json(bill)
-            }
-        })
-}
+  const error = {};
+  Bill.findOne({ user: req.user.id, isfinish: false })
+    .populate({ path: "order", model: "Order" })
+    .populate({
+      path: "order",
+      populate: {
+        path: "package_order.package_id",
+        model: "Package"
+      }
+    })
+    .exec((err, bill) => {
+      if (err) {
+        error.bills = err;
+        res.status(500).json(error);
+      } else {
+        res.json(bill);
+      }
+    });
+};
