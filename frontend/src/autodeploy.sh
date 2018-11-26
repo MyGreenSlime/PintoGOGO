0b3564e9c257c6a913811bbf2cf34cf032446020
don <ruksachai@gmail.com>
2018-11-27 00:44:52 +0700

    add autodeploy

---
+++ frontend/src/autodeploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "Please Wait for build react" 
+npm run buildfrontend
+git add -A
+git commit -m ""$(date +"%D %T buildfront")""
+git checkout deploy
+git merge backend
+git add -A
+git commit -m "$(date +"%D %T deploy")"
+git push origin deploy
+printf " ____  _       _         ____        ____\n|  _ \(_)_ __ | |_ ___  / ___| ___  / ___| ___\n| |_) | | '_ \| __/ _ \| |  _ / _ \| |  _ / _ \ \n|  __/| | | | | || (_) | |_| | (_) | |_| | (_) | \n|_|   |_|_| |_|\__\___/ \____|\___/ \____|\___/\n"
