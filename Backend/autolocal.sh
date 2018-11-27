7f7aff56edb770a6bf51285b35948aa1f80b4d44
don <ruksachai@gmail.com>
2018-11-28 03:29:02 +0700

    deploy

--- Backend/autolocal.sh
+++ Backend/autolocal.sh
@@ -9,4 +9,5 @@ echo "git commit"
 git commit -am "deploy"
 echo "git push" 
 git push origin backend
+bash deploy.sh
 printf " ____  _       _         ____        ____\n|  _ \(_)_ __ | |_ ___  / ___| ___  / ___| ___\n| |_) | | '_ \| __/ _ \| |  _ / _ \| |  _ / _ \ \n|  __/| | | | | || (_) | |_| | (_) | |_| | (_) | \n|_|   |_|_| |_|\__\___/ \____|\___/ \____|\___/\n"
