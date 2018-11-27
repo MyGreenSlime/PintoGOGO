1d9aaff60833d53e66503ed849cddc18a2064878
don <ruksachai@gmail.com>
2018-11-28 02:19:58 +0700

    deploy

--- Backend/autolocal.sh
+++ Backend/autolocal.sh
@@ -3,6 +3,10 @@ echo "Please Wait for build react"
 rm package-lock.json
 npm install
 npm run buildfrontend
-git commit -am ""$(date +"%D %T buildfront")""
+echo "git add" 
+git add -A
+echo "git commit" 
+git commit -am "deploy"
+echo "git push" 
 git push origin backend
 printf " ____  _       _         ____        ____\n|  _ \(_)_ __ | |_ ___  / ___| ___  / ___| ___\n| |_) | | '_ \| __/ _ \| |  _ / _ \| |  _ / _ \ \n|  __/| | | | | || (_) | |_| | (_) | |_| | (_) | \n|_|   |_|_| |_|\__\___/ \____|\___/ \____|\___/\n"
