9d0df2b173a8f0ebadf0a4a88969fbc6ad01b752
don <ruksachai@gmail.com>
2018-11-28 02:16:55 +0700

    config deploy&autolocal ssh

--- Backend/autolocal.sh
+++ Backend/autolocal.sh
@@ -3,7 +3,6 @@ echo "Please Wait for build react"
 rm package-lock.json
 npm install
 npm run buildfrontend
-git add -A
 git commit -am ""$(date +"%D %T buildfront")""
 git push origin backend
 printf " ____  _       _         ____        ____\n|  _ \(_)_ __ | |_ ___  / ___| ___  / ___| ___\n| |_) | | '_ \| __/ _ \| |  _ / _ \| |  _ / _ \ \n|  __/| | | | | || (_) | |_| | (_) | |_| | (_) | \n|_|   |_|_| |_|\__\___/ \____|\___/ \____|\___/\n"
--- Backend/deploy.sh
+++ Backend/deploy.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
-USERNAME = dondon
-HOST = "pintogogo.dondondiary.me"
-SCRIPT = "
-    echo '${USERNAME} : cd PintoGOGO';
-    cd PintoGOGO;
-    echo '${USERNAME} : pm2 stop 0';
-    pm2 stop 0
-    echo '${USERNAME} : git pull';
-    git pull;
-    echo '${USERNAME} : cd Backend';
-    cd Backend;
-    echo '${USERNAME} : npm install';
-    npm install;
+USERNAME="dondon"
+HOST="pintogogo.dondondiary.me"
+SCRIPT="
+echo '${USERNAME} : cd PintoGOGO';
+cd PintoGOGO;
+echo '${USERNAME} : pm2 stop 0';
+pm2 stop 0;
+echo '${USERNAME} : git pull';
+git pull;
+echo '${USERNAME} : cd Backend';
+cd Backend;
+echo '${USERNAME} : npm install';
+npm install;
+echo '${USERNAME} : pm2 start 0';
+pm2 start 0;
 "
 echo "you are about to deploy app to ${USERNAME}@${HOSTS}"
 ssh -l ${USERNAME} ${HOST} "${SCRIPT}"
