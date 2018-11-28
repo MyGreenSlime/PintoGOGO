0aac28959e87f44ad34f8863bbc763bb2526c0d8
PattarapornH <pataraporn.han@gmail.com>
2018-11-28 13:24:27 +0700

    fix path menu detail

--- frontend/src/component/recommend/recommend.js
+++ frontend/src/component/recommend/recommend.js
@@ -26,36 +26,34 @@ export default class Recommend extends Component {
       return <div />;
     }
 
-    return (
-      <section className="recommend__block">
+    return <section className="recommend__block">
         <p className="txt__rec">Recommended</p>
         <div className="row">
           <div className="col-lg-3 col-md-6 col-6 nopadding">
-            <Link to={"/menudetail/" + menus[1]._id}>
+            <Link to={"/detail/menu/" + menus[1]._id}>
               <img className="img-rec" src={menus[1].img_url} />
             </Link>
             <p>{menus[1].menu_name}</p>
           </div>
           <div className="col-lg-3 col-md-6 col-6 nopadding">
-            <Link to={"/menudetail/" + menus[1]._id}>
+            <Link to={"/detail/menu/" + menus[1]._id}>
               <img className="img-rec" src={menus[3].img_url} />
             </Link>
             <p>{menus[3].menu_name}</p>
           </div>
           <div className="col-lg-3 col-md-6 col-6 nopadding">
-            <Link to={"/menudetail/" + menus[1]._id}>
+            <Link to={"/detail/menu/" + menus[1]._id}>
               <img className="img-rec" src={menus[2].img_url} />
             </Link>
             <p>{menus[2].menu_name}</p>
           </div>
           <div className="col-lg-3 col-md-6 col-6 nopadding">
-            <Link to={"/menudetail/" + menus[1]._id}>
+            <Link to={"/detail/menu/" + menus[1]._id}>
               <img className="img-rec" src={menus[0].img_url} />
             </Link>
             <p>{menus[0].menu_name}</p>
           </div>
         </div>
-      </section>
-    );
+      </section>;
   }
 }
