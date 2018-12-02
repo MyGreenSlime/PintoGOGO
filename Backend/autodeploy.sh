4c75777fcbcf212919bb4ecaf2bb5e88e83b6647
don <ruksachai@gmail.com>
2018-12-02 22:51:35 +0700

    add auto deploy

---
+++ Backend/autodeploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "Please Wait for build react" 
+rm package-lock.json
+npm install
+npm run buildfrontend
+echo "git add" 
+git add -A
+echo "git commit" 
+git commit -am "deploy `date +'%Y-%m-%d %H:%M:%S'`"
+echo "git push" 
+git push origin backend
+bash deploy.sh
+printf " ____  _       _         ____        ____\n|  _ \(_)_ __ | |_ ___  / ___| ___  / ___| ___\n| |_) | | '_ \| __/ _ \| |  _ / _ \| |  _ / _ \ \n|  __/| | | | | || (_) | |_| | (_) | |_| | (_) | \n|_|   |_|_| |_|\__\___/ \____|\___/ \____|\___/\n"
